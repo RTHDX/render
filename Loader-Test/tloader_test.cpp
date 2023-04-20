3151e2add988f401b668d901a6f3efca942a14bd
Roman Bikhtiaiev <Roman.Bikhtiaiev@protonmail.ch>
2023-04-20 16:36:48 +0300

    [Render] Remove assimp usage

--- Loader-Test/tloader_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-#include <Loader/tloader.hpp>
-
-
-TEST(Loader, load_scene) {
-    loader::TLoader loader;
-    ASSERT_TRUE(loader.read(std::filesystem::path("cube.gltf")));
-}
